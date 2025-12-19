ae27a7458d622bb55996f1dc78f74b5969afff37
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-12-19 18:30:33 +0800

    feat: Add page load for nav buttons
    Menu, and contact pages not yet created

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ import "./styles.css";
 import loadHomePage from "./scripts/home";
 
 loadHomePage();
+
+const nav = document.querySelector("nav")
+
+nav.addEventListener("click", (e) => {
+    const navText = e.target.textContent
+
+    switch (navText) {
+        case "Home":
+            loadPage(loadHomePage);
+            break;
+        case "Menu":
+            console.log("Menu pressed");
+            break;
+        case "Contact Us":
+            console.log("Contact Us pressed");
+            break;
+    }
+})
+
+function loadPage(page) {
+    const content = document.querySelector(".content")
+    content.innerHTML = "";
+
+    page();
+}
