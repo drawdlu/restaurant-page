754645b7755174baa1a362e834e60b32ff1ba361
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-12-29 23:16:03 +0800

    style: Change active button styles when clicked

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import loadMenuPage from "./scripts/menu";
 loadHomePage();
 
 const nav = document.querySelector("nav")
-const activeTabButton = document.querySelector("nav ul li:nth-child(1) button");
+let activeTabButton = document.querySelector("nav ul li:nth-child(1) button");
 activeTabButton.classList.add("active");
 
 nav.addEventListener("click", (e) => {
@@ -22,7 +22,26 @@ nav.addEventListener("click", (e) => {
             console.log("Contact Us pressed");
             break;
     }
+
+    changeActiveButton(e);
 })
+
+function changeActiveButton(e) {
+    const elementName = e.target.tagName;
+
+    if (elementName === "BUTTON" || elementName === "SPAN") {
+        let targetButton;
+        if (elementName === "BUTTON") {
+            targetButton = e.target;
+        } else {
+            targetButton = e.target.parentElement;
+        }
+
+        activeTabButton.classList.remove("active")
+        activeTabButton = targetButton
+        activeTabButton.classList.add("active")
+    }
+}
 
 function loadPage(page) {
     const content = document.querySelector(".content")
